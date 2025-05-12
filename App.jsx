61cc115ff087310fb42befc1f3bf0d402a40f424
Mikhael Edman Gomez <edman.gomez.business@gmail.com>
2025-05-12 09:50:32 +0800

    fix

--- App.jsx
+++ App.jsx
@@ -6,13 +6,6 @@ import { useWindowSize } from "react-use"
 import Confetti from "react-confetti"
 
 
-/**
- * Goal: Build out the main parts of our app
- * 
- * Challenge: Add a header with the game title
- * and description. Startin' out easy 🙂🚶‍➡️
- */
-
 export default function App() {
 
     //Constant values
